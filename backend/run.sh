a1ef067fc31ac027359d7101b580f73ea96cd414
kinow83 <kinow83@gmail.com>
2020-11-03 14:30:25 +0000

    소스 변경 시 gunicorn 자동 reload

--- backend/run.sh
+++ backend/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo mkdir -p /var/log/gunicorn
 sudo touch /var/log/gunicorn/error.log
-sudo PYTHONPATH=.  gunicorn app.wsgi -c gunicorn/gunicorn.py 
+sudo PYTHONPATH=.  gunicorn app.wsgi -c gunicorn/gunicorn.py --reload
